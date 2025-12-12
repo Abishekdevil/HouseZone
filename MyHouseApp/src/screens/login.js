33832428a3c70c391436ba24cdda12a87b00ffc2
Abishekdevil <abishekcholan07@gmail.com>
2025-12-12 08:01:48 +0530

    merge conflicts error and functionality all are working properly

--- MyHouseApp/src/screens/login.js
+++ MyHouseApp/src/screens/login.js
@@ -47,44 +47,46 @@ export default function Login() {
 
   return (
     <View style={loginStyles.container}>
-      <View style={categoryContentStyles.formContainer}>
-        <Text style={categoryContentStyles.formTitle}>Login</Text>
-        <Text style={categoryContentStyles.label}>Name</Text>
-        <TextInput
-          style={categoryContentStyles.input}
-          placeholder="Name"
-          value={name}
-          onChangeText={setName}
-        />
-        <Text style={categoryContentStyles.label}>Phone Number</Text>
-        <TextInput
-          style={categoryContentStyles.input}
-          placeholder="Phone Number"
-          keyboardType="phone-pad"
-          value={phone}
-          onChangeText={setPhone}
-        />
-        <Text style={categoryContentStyles.label}>Password</Text>  
-        <TextInput
-          style={categoryContentStyles.input}
-          placeholder="Password"
-          secureTextEntry
-          value={password}
-          onChangeText={setPassword}
-        />
+      <View style={{ flex: 1, justifyContent: 'center', width: '100%', maxWidth: 400, paddingHorizontal: 20 }}>
+        <View style={categoryContentStyles.formContainer}>
+          <Text style={categoryContentStyles.formTitle}>Login</Text>
+          <Text style={categoryContentStyles.label}>Name</Text>
+          <TextInput
+            style={categoryContentStyles.input}
+            placeholder="Name"
+            value={name}
+            onChangeText={setName}
+          />
+          <Text style={categoryContentStyles.label}>Phone Number</Text>
+          <TextInput
+            style={categoryContentStyles.input}
+            placeholder="Phone Number"
+            keyboardType="phone-pad"
+            value={phone}
+            onChangeText={setPhone}
+          />
+          <Text style={categoryContentStyles.label}>Password</Text>  
+          <TextInput
+            style={categoryContentStyles.input}
+            placeholder="Password"
+            secureTextEntry
+            value={password}
+            onChangeText={setPassword}
+          />
 
-        <TouchableOpacity 
-          style={[categoryContentStyles.button, categoryContentStyles.primaryButton]} 
-          onPress={handleLogin}
-        >
-          <Text style={categoryContentStyles.buttonText}>Login</Text>
-        </TouchableOpacity>
-
-        <View style={{ marginTop: 15, flexDirection: "row", justifyContent: 'center' }}>
-          <Text>Don't have an account? </Text>
-          <TouchableOpacity onPress={() => navigation.navigate("Signup")}>
-            <Text style={{ color: "blue" }}>Signup</Text>
+          <TouchableOpacity 
+            style={[categoryContentStyles.button, categoryContentStyles.primaryButton]} 
+            onPress={handleLogin}
+          >
+            <Text style={categoryContentStyles.buttonText}>Login</Text>
           </TouchableOpacity>
+
+          <View style={{ marginTop: 15, flexDirection: "row", justifyContent: 'center' }}>
+            <Text>Don't have an account? </Text>
+            <TouchableOpacity onPress={() => navigation.navigate("Signup")}>
+              <Text style={{ color: "blue" }}>Signup</Text>
+            </TouchableOpacity>
+          </View>
         </View>
       </View>
     </View>
